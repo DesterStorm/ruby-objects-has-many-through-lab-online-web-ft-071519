c51196caed87c745792399c2f13f84d7635bf9d1
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-21 16:59:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,13 @@
 class Song
   attr_accessor :name, :artist, :genre
-  
+  @@all = []
   def initialize(name, genre, artist)
     @name = name
     @genre = genre
     genre.add_song(self)
+  end
+  
+  def self.all 
+    @@all 
   end
 end 
