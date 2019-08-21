a1ad9a2e008fa4407779cf4da12c47de4d8dc64b
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-21 17:13:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,40 +1,3 @@
-# class Artist
-#   attr_accessor :name
-  
-#   @@all = []
-  
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#     @@all << self
-#   end
- 
-#   def add_song(song)
-#     @songs << song
-#     song.artist = self
-#   end
-  
-#   def self.all 
-#     @@all
-    
-#   end
-  
-#   def songs
-#     Song.all.select do |song|
-#       song.artist = self
-#     end
-#   end
-  
-#   def new_song(name, genre)
-#     new_song = Song.new(name, self, genre)
-#   end
-  
-#   def genres 
-#     self.songs.collect {|song| song.genre}
-#   end
-# end
-
-
 
 class Artist 
 
@@ -61,7 +24,6 @@ class Artist
    def new_song(song, genre)
     Song.new(name, self, genre)
   end
-
 
  end 
 
