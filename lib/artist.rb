31abbb5b488c6f26e07b07d2378b4f2ff0e0b08c
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-21 17:11:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,38 +1,67 @@
-class Artist
-  attr_accessor :name
+# class Artist
+#   attr_accessor :name
   
+#   @@all = []
+  
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#     @@all << self
+#   end
+ 
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#   end
+  
+#   def self.all 
+#     @@all
+    
+#   end
+  
+#   def songs
+#     Song.all.select do |song|
+#       song.artist = self
+#     end
+#   end
+  
+#   def new_song(name, genre)
+#     new_song = Song.new(name, self, genre)
+#   end
+  
+#   def genres 
+#     self.songs.collect {|song| song.genre}
+#   end
+# end
+
+
+
+class Artist 
+
+   attr_accessor :name
   @@all = []
-  
-  def initialize(name)
+
+   def initialize(name)
     @name = name
-    @songs = []
     @@all << self
   end
- 
-  def add_song(song)
-    @songs << song
-    song.artist = self
+
+   def songs
+    Song.all.select {|song| song.artist == self}
   end
-  
-  def self.all 
+
+   def genres
+    songs.map {|song| song.genre}
+  end
+
+   def self.all
     @@all
-    
   end
-  
-  def songs
-    Song.all.select do |song|
-      song.artist = self
-    end
+
+   def new_song(song, genre)
+    Song.new(name, self, genre)
   end
-  
-  def new_song(name, genre)
-    new_song = Song.new(name, self, genre)
-  end
-  
-  def genres 
-    self.songs.collect {|song| song.genre}
-  end
-end
 
 
+ end 
 
